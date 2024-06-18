a022c5c7d386c000e5b4d62ff4dc7e135a7ff38c
Iliatar <a.u.tarasov@gmail.com>
2024-06-18 21:37:41 +0300

    add dynamic routes

--- HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
+++ HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
@@ -10,10 +10,26 @@ public class HelloWorld {
 
         app.get("/users", ctx -> ctx.result("GET/Users"));
         app.post("/users", ctx -> ctx.result("POST/Users"));
+
         app.get("/hello", ctx -> {
            String name = ctx.queryParam("name");
            ctx.result("Hello, " + name + "!");
         });
+
+        app.get("/courses/{Id}", ctx -> {
+           ctx.result("CourseId: " + ctx.pathParam("Id"));
+        });
+
+        app.get("/users/{Id}", ctx -> {
+            ctx.result("UserId: " + ctx.pathParam("Id"));
+        });
+
+        app.get("users/{id}/post/{postId}", ctx -> {
+            String userId = ctx.pathParam("id");
+            String postId = ctx.pathParam("postId");
+            ctx.result("User id: " + userId + "; Post id: " + postId);
+        });
+
         app.start(7070);
     }
 }
