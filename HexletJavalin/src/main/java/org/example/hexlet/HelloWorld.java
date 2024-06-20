8c107a573cb95cd98085af22f7d2683185ce1a0c
Iliatar <a.u.tarasov@gmail.com>
2024-06-20 22:09:18 +0300

    add layout

--- HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
+++ HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,33 +1,40 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import org.example.hexlet.dto.courses.CoursePage;
+import org.example.hexlet.model.Course;
+
+import java.util.List;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
         });
 
-        app.get("/users", ctx -> ctx.result("GET/Users"));
-        app.post("/users", ctx -> ctx.result("POST/Users"));
+        Course course1 = new Course(1l, "Java", "Java developer");
+        Course course2 = new Course(2l, "Frontend", "Frontend Developer");
+        Course course3 = new Course(3l, "DBA", "Database Administrator");
+        CoursePage coursePage = new CoursePage(List.of(course1, course2, course3), "Курсы по программированию");
 
-        app.get("/hello", ctx -> {
-           String name = ctx.queryParam("name");
-           ctx.result("Hello, " + name + "!");
+        app.get("/courses/", ctx -> {
+            ctx.render("summary.jte", model("coursePage", coursePage));
         });
 
-        app.get("/courses/{Id}", ctx -> {
-           ctx.result("CourseId: " + ctx.pathParam("Id"));
-        });
-
-        app.get("/users/{Id}", ctx -> {
-            ctx.result("UserId: " + ctx.pathParam("Id"));
-        });
-
-        app.get("users/{id}/post/{postId}", ctx -> {
-            String userId = ctx.pathParam("id");
-            String postId = ctx.pathParam("postId");
-            ctx.result("User id: " + userId + "; Post id: " + postId);
+        app.get("/courses/{id}", ctx -> {
+            Long courseId = ctx.pathParamAsClass("id", Long.class).get();
+            Course course = null;
+            for (Course c : coursePage.getCourses()) {
+                if (c.getId() == courseId) {
+                    course = c;
+                    break;
+                }
+            }
+            ctx.render("course.jte", model("course", course));
         });
 
         app.start(7070);
